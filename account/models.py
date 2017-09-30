34e30441ea7ea3fa926ba6ca53f81d53b210163a
zhangzongxiang <zhangzongxiang@yihecloud.com>
2017-09-30 16:42:57 +0800

    user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 # Create your models here.
@@ -9,15 +10,24 @@ class User(models.Model):
         (FEMALE, 'female')
     )
 
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
+    id = models.CharField(primary_key=True,
+                          max_length=32,
+                          default=uuid.uuid4,
+                          unique=True,
+                          editable=False)
+    username = models.SlugField()
     password = models.CharField(max_length=50)
-    gender = models.BooleanField(choices=GENDER_CHOICES, null=True)
-    email = models.CharField(max_length=255, null=True)
+    nick = models.CharField(max_length=200, null=True)
+    gender = models.NullBooleanField(choices=GENDER_CHOICES, null=True)
+    email = models.EmailField(null=True)
     phone = models.CharField(max_length=50, null=True)
     qq = models.CharField(max_length=50, null=True)
     address = models.CharField(max_length=1000, null=True)
+    remark = models.TextField(null=True)
+
+class UserSign(models.Model):
+    user = models.ForeignKey(User)
+    sign_up_num = models.AutoField(primary_key=False)
     sign_up_at = models.DateTimeField(auto_now_add=True)
     sign_in_last = models.DateTimeField(null=True)
     sign_in_err = models.IntegerField(default=0)
-
