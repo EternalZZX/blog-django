dc4cc7d441c95b540f6cc440a0ecd9ce5f8577cb
EternalZZX <1301340187@qq.com>
2018-06-02 17:54:35 +0800

    wechat media

--- blog/wechat/common/media.py
+++ blog/wechat/common/media.py
@@ -4,6 +4,7 @@
 import os
 import json
 import urllib2
+import requests
 
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
@@ -18,10 +19,12 @@ class Media(object):
 
     def photo_upload(self, url):
         image_path = os.path.join(MEDIA_ROOT, url.replace(MEDIA_URL, ''))
-        image_data = open(image_path, "rb").read()
+        image_data = open(image_path, "rb")
         post_url = "https://api.weixin.qq.com/cgi-bin/media/upload?" \
                    "access_token=%s&type=image" % AccessService.get_access_token()
         post_data, post_headers = multipart_encode({'media': image_data})
-        request = urllib2.Request(post_url, post_data, post_headers)
-        response = urllib2.urlopen(request)
-        return json.loads(response.read())['mediaId']
+        # request = urllib2.Request(post_url, post_data, post_headers)
+        # response = urllib2.urlopen(request)
+        # return json.loads(response.read())['media_id']
+        response = requests.post(post_url, files=post_data, headers=post_headers)
+        return json.loads(response.content)['media_id']
